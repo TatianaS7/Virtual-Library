ad96aacd3c80ffa899a42900c972b694442c6dce
Zachary Harris <the1zachharris@gmail.com>
2024-02-06 11:33:55 -0600

    created media class and included it in main.js

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,16 @@
 // create your Media class:
+class Media {
+    static totalMediaCount = 0
+    constructor(title,genre,year) {
+        this.title = title
+        this.genre = genre
+        this.year = year
+        totalMediaCount++ 
+    }
+    summary(){
+        return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`
+    }
 
+}
 // uncomment below to export it:
 module.exports = Media;
