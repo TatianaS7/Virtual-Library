538c22d7b7e865fc9bb05fff829e37dd211af8e1
Tatiana Stewart <tstwrt7@gmail.com>
2024-02-06 12:52:47 -0500

    initial commit

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,22 @@
 // import the Media class:
+const Media = require('./Media.js')
 
 // create your Book class:
+class Book extends Media {
+    constructor(title, genre, year, author, numPages, rating) {
+        super(title, genre, year);
+
+        this.author = author
+        this.numPages = numPages
+        this.rating = rating
+    }
+    summary() {
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+    static highestRating(books) {
+        return Math.max(books);
+    }
+}
 
 // don't change below
 module.exports = Book;
--- main.js
+++ main.js
@@ -1,1 +1,2 @@
 // Import classes here to console.log and debug
+const Book = require('./classes/Book');
